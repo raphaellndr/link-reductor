9bf8251e0b942fbe618e40727b5f9fa156b26406
raphaellndr <raph.landure@gmail.com>
2026-01-06 21:38:54 +0100

    Fix settings (use config folder instead of app)

--- back/config/settings.py
+++ back/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "app.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "app.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
